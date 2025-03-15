62e9f62c79ee65cdb702081f7cdbed9fed3f1398
Deepak Kathayat <2456943+dmkxyz@users.noreply.github.com>
2025-03-15 12:05:12 +0400

    define the deriv procedure

--- chapter-2/e2.56.rkt
+++ chapter-2/e2.56.rkt
@@ -1,7 +1,7 @@
 #lang sicp
 (define (variable? x) (symbol? x))
 
-(define (same-variable a1 a2)
+(define (same-variable? a1 a2)
   (and (variable? a1) (variable? a2) (eq? a1 a2)))
 
 (define (sum? e)
@@ -37,3 +37,19 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
