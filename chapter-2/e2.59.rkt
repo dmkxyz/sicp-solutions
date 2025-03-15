0b5c8014840181f9f7caf6df54b1f9e5a32a4277
Deepak Kathayat <2456943+dmkxyz@users.noreply.github.com>
2025-03-15 14:58:09 +0400

    add union-set operation

--- chapter-2/e2.59.rkt
+++ chapter-2/e2.59.rkt
@@ -17,3 +17,8 @@
                (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
 
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (cons (car set1)
+                    (union-set (cdr set1) set2)))))
