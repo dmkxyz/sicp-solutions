337b75af3833d3b83f28351f3a70a601838a761d
Deepak Kathayat <2456943+dmkxyz@users.noreply.github.com>
2025-03-15 12:49:54 +0400

    extend to handle sums & products of arbitrary number of terms

---
+++ chapter-2/e2.57.rkt
@@ -0,0 +1,82 @@
+#lang sicp
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? a1 a2)
+  (and (variable? a1) (variable? a2) (eq? a1 a2)))
+
+(define (sum? e)
+  (and (pair? e) (eq? (car e) '+)))
+
+(define (addend e)
+  (cadr e))
+
+(define (augend e)
+  (accumulate make-sum 0 (cddr e)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (product? e)
+  (and (pair? e) (eq? (car e) '*)))
+
+(define (multiplier e)
+  (cadr e))
+
+(define (multiplicand e)
+  (accumulate make-product 1 (cddr e)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-exponentiation base exponent)
+  (cond ((= exponent 0) 1)
+        ((= exponent 1) base)
+        ((= base 1) 1)
+        (else (list '** base exponent))))
+
+(define (exponentiation? exp)
+  (and (pair? exp) (eq? (car exp) '**)))
+
+(define (base exp)
+  (cadr exp))
+
+(define (exponent exp)
+  (caddr exp))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (deriv (base exp) var)
+                       (make-product (exponent exp)
+                                     (make-exponentiation (base exp) (- (exponent exp) 1)))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+
