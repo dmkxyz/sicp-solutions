d2c3a1aedb7e07fd9e563bd776fb3da7e68f3a7c
Deepak Kathayat <2456943+dmkxyz@users.noreply.github.com>
2025-03-15 15:23:40 +0400

    add union-set for ordered sets

---
+++ chapter-2/e2.62.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(define (union-set set1 set2)
+  (accumulate adjoin-set set2 set1))
