82241574ae263a7f3bce8b41e9a8fbf733c6228a
Deepak Kathayat <2456943+dmkxyz@users.noreply.github.com>
2025-03-15 11:46:52 +0400

    add a few other predicates, plus selection & constructor for sum

--- chapter-2/e2.56.rkt
+++ chapter-2/e2.56.rkt
@@ -1,3 +1,24 @@
 #lang sicp
 (define (variable? x) (symbol? x))
 
+(define (same-variable a1 a2)
+  (and (variable? a1) (variable? a2) (eq? a1 a2)))
+
+(define (sum? e)
+  (eq? (car e) '+))
+
+(define (addend e)
+  (cadr e))
+
+(define (augend e)
+  (caddr e))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
